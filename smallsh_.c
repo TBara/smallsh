fa6fcb098a64c815e94433c6507a0919c32ab032
Tomas Barabasz <barabast@os1.engr.oregonstate.edu>
2020-05-12 22:47:13 -0700

    needs signals work

--- smallsh_.c
+++ smallsh_.c
@@ -11,31 +11,55 @@
 #define BUFFER_MAX 2048
 #define ARG_MAX 513
 const char *AMP = "&";
+volatile sig_atomic_t B_GND = 0; 
+volatile sig_atomic_t PID_CNT = 0;
+pid_t PID_ARR[ARG_MAX];
 
-void catchSIGINT(int signo);
+// Prototypes
+void add_pid_arr(pid_t status);
+void del_pid_arr();
+void chk_pid_arr();
+void sigint_handler(int signum);
+void sigchld_handler(int signum);
+void sigtstp_handler(int signum);
 void parseInput(char *input, char *arg_arr[], int *arg_cnt, char *input_file, char *outputfile);
 bool isBackground(char *arg_arr[], int *arg_cnt);
 void chng_dir(char *arg_arr[]);
 void process_cmd(char *arg_arr[], int *arg_cnt, int *status);
-void getStatus(int *child_status);
-void start_foreground(char *arg_arr[], char *input_file, char *outputfile);
+void getStatus(pid_t *child_status);
+void start_foreground(char *arg_arr[], char *input_file, char *outputfile, int *status);
 void start_background(char *arg_arr[], char *input_file, char *outputfile);
+void set_input_fd_bg(int *sourceFD, int *result, char *input_file);
+void set_output_fd_bg(int *targetFD, int *result, char *output_file);
+void set_input_fd_fg(int *sourceFD, int *result, char *input_file);
+void set_output_fd_fg(int *targetFD, int *result, char *output_file);
 
+// Main driver function
 void main()
 {
     char *arg_arr[ARG_MAX]; // Store passed in arguments
     int arg_cnt = 0;
     
     struct sigaction SIGINT_action = {0};
-    SIGINT_action.sa_handler = catchSIGINT;
+    SIGINT_action.sa_handler = sigint_handler;
+    SIGINT_action.sa_handler = sigchld_handler;
     sigfillset(&SIGINT_action.sa_mask);
     //SIGINT_action.sa_flags = SA_RESTART;
     sigaction(SIGINT, &SIGINT_action, NULL);
+    sigaction(SIGCHLD, &SIGINT_action, NULL);
 
-    int numCharsEntered = -5; // How many chars we entered
-    int status = 0;
-    size_t buffer_size = 0;   // Holds how large the allocated buffer is
-    char *userInput = NULL;   // Points to a buffer allocated by getline() that holds our entered string + \n + \0
+    struct sigaction sig_tstp={0};	
+	sig_tstp.sa_handler = &sigtstp_handler;
+	sigfillset(&sig_tstp.sa_mask);
+	//sig_tstp.sa_flags=SA_RESTART;
+	sigaction(SIGTSTP, &sig_tstp, NULL); 
+
+    int numCharsEntered = -5;
+    int stat = 0;
+    int *status;
+    status = &stat;
+    size_t buffer_size = 0;   
+    char *userInput = NULL;  
 
     while (1)
     {
@@ -45,7 +69,11 @@ void main()
         // Get input from the user
         while (1)
         {
-            printf("Enter command:");
+            fflush(stdout);
+            printf(": ");
+            fflush(stdout);
+            //write(STDOUT_FILENO, ": ", 2);
+            fflush(stdout);
             numCharsEntered = getline(&userInput, &buffer_size, stdin); // Get a line from the user
             if (numCharsEntered == -1) {
                 clearerr(stdin);
@@ -59,30 +87,35 @@ void main()
         parseInput(userInput, arg_arr, &arg_cnt, input_file, output_file); // Tokenizes the input and stores it in arg_arr
 
         if ((arg_cnt > 0) && (strcmp(arg_arr[0], "cd") == 0)){
-            chng_dir(arg_arr);
+            chng_dir(arg_arr);  /// Navigate dirs
 
         } else if ((arg_cnt > 0) && (strcmp(arg_arr[0], "exit") == 0)) {
-            exit(EXIT_SUCCESS);
+            exit(EXIT_SUCCESS); // Exit program
 
         } else if ((arg_cnt > 0) && (strcmp(arg_arr[0], "status") == 0)) {
-            getStatus(&status);
+            getStatus(status); // Status of last foreground process
 
-        } else if ((arg_cnt > 0) && isBackground(arg_arr, &arg_cnt)){
-            start_background(arg_arr, input_file, output_file);
+        } else {
+            // Check the globla variable if foreground-only is enabled
+            if(B_GND == 0){
+                // Run in either mode
+                if ((arg_cnt > 0) && isBackground(arg_arr, &arg_cnt)){
+                    start_background(arg_arr, input_file, output_file);
+                } else if ((arg_cnt > 0) && !isBackground(arg_arr, &arg_cnt)) {
+                    start_foreground(arg_arr, input_file, output_file, status);
+                }
 
-        } else if ((arg_cnt > 0) && !isBackground(arg_arr, &arg_cnt)) {
-            start_foreground(arg_arr, input_file, output_file);
+            } else if(B_GND == 1){
+                // Run in foreground no matter what
+                if ((arg_cnt > 0) && isBackground(arg_arr, &arg_cnt)){
+                    start_foreground(arg_arr, input_file, output_file, status);
+                } else if ((arg_cnt > 0) && !isBackground(arg_arr, &arg_cnt)) {
+                    start_foreground(arg_arr, input_file, output_file, status);
+                }             
+            }            
         }
 
-        /*************** Delete after testing ********/
-        // int i;
-        // printf("Args: %d\n", arg_cnt);
-        // printf("In_f: %s\n", input_file);
-        // printf("Out_f: %s\n", output_file);
-        // for (i = 0; i < arg_cnt; i++){
-        //     printf("Arg%d: %s\n", i, arg_arr[i]);
-        // }
-        /*********************************************/
+        chk_pid_arr();
 
         // Free the memory allocated by getline() or else memory leak
         free(input_file);
@@ -92,83 +125,39 @@ void main()
     }
 }
 
-void start_foreground(char *arg_arr[], char *input_file, char *outputfile){
-    printf("Run in foreground\n");
-
-    pid_t spawnPid = -5;
-    pid_t actualPid = -5;
-    int childExitStatus = -5;
-    spawnPid = fork(); // Fork new process
-    
-    switch (spawnPid)
-    {
-        case -1: { 
-            // Unknown error
-            perror("Something went wrong when forking\n"); 
-            exit(1); 
-            break;
-            }
-        case 0: {
-            // Execute the command with params
-            execvp(*arg_arr, arg_arr);
-            perror("Exec failure\n");
-            exit(2);
-            break;
-        }
-        default:
-        // Check if any process has completed, block parent until child process terminates
-        actualPid = waitpid(spawnPid, &childExitStatus, 0);
-        break;
-    }
-
-}
-
 //
-void set_input_fd(int *sourceFD, int *result, char *input_file){
-    if (strlen(input_file) < 1)
-    {
-        strcpy(input_file, "/dev/null");
-    }
-    
-    *sourceFD = open(input_file, O_RDONLY);
-    if (*sourceFD == -1) { 
-        perror("source open()"); 
-        exit(1); 
-    }
-
-    *result = dup2(*sourceFD, 0);
-    if (*result == -1) { 
-        perror("source dup2()"); 
-        exit(2); 
+void add_pid_arr(pid_t pid){
+    if(PID_CNT < (ARG_MAX - 1)){
+        PID_ARR[PID_CNT] = pid;
+        PID_CNT++;
     }
 }
 
 //
-void set_output_fd(int *targetFD, int *result, char *output_file){
-    if (strlen(output_file) < 1)
-    {
-        strcpy(output_file, "/dev/null");
-    }
-    
-    *targetFD = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (*targetFD == -1) { 
-        perror("target open()"); 
-        exit(1); 
-    }
-
-    *result = dup2(*targetFD, 1);
-    if (*result == -1) { 
-        perror("target dup2()"); 
-        exit(2); 
+void del_pid_arr(){
+    if(PID_CNT < 0){
+        PID_ARR[PID_CNT] = 0;
+        PID_CNT--;
     }
 }
 
-void start_background(char *arg_arr[], char *input_file, char *output_file){
-    printf("Run in background\n");
+//
+void chk_pid_arr(){
+    while (PID_CNT < 0)
+    {
+        pid_t *status = &PID_ARR[PID_CNT];
+        getStatus(status);
+        del_pid_arr(PID_ARR[PID_CNT]);
+    }
 
+}
+
+// Run processes in the foreground, parant waits for child to finish
+void start_foreground(char *arg_arr[], char *input_file, char *output_file, int *status){
+    //printf("Run in foreground\n");
     int sourceFD, targetFD, result;
     pid_t spawnPid = -5;
-    pid_t actualPid = -5;
+    pid_t childPid = -5;
     int childExitStatus = -5;
     spawnPid = fork(); // Fork new process
     
@@ -177,40 +166,176 @@ void start_background(char *arg_arr[], char *input_file, char *output_file){
         case -1: { 
             // Some error happened
             perror("Something went wrong when forking\n"); 
-            exit(1); 
+            exit(2); 
             break;
             }
         case 0: {
-            // 
-            set_input_fd(&sourceFD, &result, input_file);
-            set_output_fd(&targetFD, &result, output_file);
+            // Do any input/output redirection
+            set_input_fd_fg(&sourceFD, &result, input_file);
+            set_output_fd_fg(&targetFD, &result, output_file);
 
             // Execute the command with params
             execvp(*arg_arr, arg_arr);
-            perror("Exec failure\n");
-            exit(2);
+            
+            // This should not execute
+            perror("");
+            exit(1);
             break;
         }
         default:
-        // Check if the process specified has completed, return immediately with 0 if it hasn’t
-        actualPid = waitpid(spawnPid, &childExitStatus, WNOHANG);
+        //Parent; Check if any process has completed, block parent until child process terminates
+        childPid = waitpid(spawnPid, &childExitStatus, 0);
+        *status = childExitStatus;
         break;
     }
-    close(sourceFD);
+
 }
 
-void getStatus(int *child_status)
+// Run processes in the background, parant does not wait for child to finish
+void start_background(char *arg_arr[], char *input_file, char *output_file)
 {
-    if (WIFEXITED(child_status) != 0)
+    int sourceFD, targetFD, result;
+    pid_t spawnPid = -5;
+    pid_t childPid = -5;
+    int childExitStatus = -5;
+    spawnPid = fork();
+    switch (spawnPid)
+    {
+    case -1:
+    {
+        perror("Hull Breach!\n");
+        exit(1);
+        break;
+    }
+    case 0:
+    {
+        printf("background pid is %d\n", getpid());
+        fflush(stdout);
+
+        // Do any file redirects
+        set_input_fd_bg(&sourceFD, &result, input_file);
+        set_output_fd_bg(&targetFD, &result, output_file);
+
+        // Execute the command with params
+        execvp(*arg_arr, arg_arr);
+        perror("CHILD: exec failure!\n");
+        exit(2);
+        break;
+    }
+    default:
+        {
+            // Don't wait for child to finish
+            childPid = waitpid(spawnPid, &childExitStatus,  WNOHANG);
+            break;
+        }
+    }
+    // Close files
+    close(sourceFD);
+    close(targetFD);
+}
+
+// Set input file descriptor for background processes
+void set_input_fd_bg(int *sourceFD, int *result, char *input_file){
+    // If no file path supplied, send to black hole
+    if (strlen(input_file) < 1)
+    {
+        strcpy(input_file, "/dev/null");
+    }
+    
+    // If file path is supplied, open
+    *sourceFD = open(input_file, O_RDONLY);
+    if (*sourceFD == -1) { 
+        perror(""); 
+        exit(1); 
+    }
+
+    // set input, check for error
+    *result = dup2(*sourceFD, 0);
+    if (*result == -1) { 
+        perror(""); 
+        exit(2); 
+    }
+}
+
+// Set output file descriptor for background processes
+void set_output_fd_bg(int *targetFD, int *result, char *output_file){
+    // If no file path supplied, send to black hole
+    if (strlen(output_file) < 1)
+    {
+        strcpy(output_file, "/dev/null");
+    }
+    
+    // If file path is supplied, open or create it, in overwrite mode, set permissions
+    *targetFD = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (*targetFD == -1) { 
+        perror(""); 
+        exit(1); 
+    }
+
+    // set output, check for error
+    *result = dup2(*targetFD, 1);
+    if (*result == -1) { 
+        perror(""); 
+        exit(2); 
+    }
+}
+
+// Set input file descriptor for foreground processes
+void set_input_fd_fg(int *sourceFD, int *result, char *input_file){
+    if (strlen(input_file) > 0)
+    {
+        // If file path is supplied, open
+        *sourceFD = open(input_file, O_RDONLY);
+        if (*sourceFD == -1) { 
+            perror("cannot open badfile for input"); 
+            exit(1); 
+        }
+
+        // Set input, check for error
+        *result = dup2(*sourceFD, 0);
+        if (*result == -1) { 
+            perror("source dup2()"); 
+            exit(2); 
+        }
+    }
+}
+
+// Set output file descriptor for foreground processes
+void set_output_fd_fg(int *targetFD, int *result, char *output_file){
+    if (strlen(output_file) > 0)
+    {
+        // If file path is supplied, open or create it, in overwrite mode, set permissions
+        *targetFD = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (*targetFD == -1) { 
+            perror("target open()"); 
+            exit(1); 
+        }
+
+        // set output, check for error
+        *result = dup2(*targetFD, 1);
+        if (*result == -1) { 
+            perror("target dup2()"); 
+            exit(2); 
+        }
+        
+    }
+    
+
+}
+
+// Checks the status of process that's finished
+void getStatus(pid_t *child_status)
+{
+    if (WIFEXITED(*child_status) != 0)
     {
         // get status after normal exit
-        printf("exit value %d\n", (int)WEXITSTATUS(child_status));
+        printf("exit value %d\n", (int)WEXITSTATUS(*child_status));
         fflush(stdout);
     }
-    else if (WIFSIGNALED(child_status) != 0)
+    else if (WIFSIGNALED(*child_status) != 0)
     {
         // get exit signal
-        printf("terminated by signal %d\n", (int)WTERMSIG(child_status));
+        printf("\nterminated by signal %d\n", (int)WTERMSIG(*child_status));
         fflush(stdout);
     }
 }
@@ -221,11 +346,11 @@ void chng_dir(char *arg_arr[])
     char *dir;
     if (arg_arr[1] == NULL)
     {
-        // No path supplied
+        // No path supplied, get path to home from env vars
         dir = getenv("HOME");
         if (chdir(dir) != 0)
         {
-            perror("cd command failed to execute.\n");
+            perror("cd command failed\n");
         }
     }
     else if ((arg_arr[1] != NULL) && (arg_arr[2] == NULL))
@@ -234,15 +359,9 @@ void chng_dir(char *arg_arr[])
         dir = arg_arr[1];
         if (chdir(dir) != 0)
         {
-            perror("cd command failed to execute.\n");
+            perror("cd command failed.\n");
         }
     }
-    // else {
-    //     // Do something??? Too many args
-    // }
-
-    char s[100]; // Delete
-    printf("cwd: %s\n", getcwd(s, 100));
 }
 
 // Returns true if the last argument is &
@@ -250,14 +369,13 @@ bool isBackground(char *arg_arr[], int *arg_cnt)
 {
     if (strcmp(arg_arr[(*arg_cnt) - 1], AMP) == 0)
     {
-        //printf("& found\n");
+        // Remove the & from arg_arr
         arg_arr[(*arg_cnt) - 1] = NULL;
         (*arg_cnt)--;
         return true;
     }
     else
     {
-        //printf("& NOT found\n");
         return false;
     }
 }
@@ -277,15 +395,18 @@ void parseInput(char *input, char *arg_arr[], int *arg_cnt, char *input_file, ch
     {
         if (strncmp(token, "#", 1) == 0)
         {
+            // Ignore
             return;
         }
         else if (strcmp(token, ">") == 0)
         {
+            // next token is a file path
             token = (strtok(NULL, " "));
             strcpy(output_file, token);
         }
         else if (strcmp(token, "<") == 0)
         {
+            // next token is a file path
             token = (strtok(NULL, " "));
             strcpy(input_file, token);
         }
@@ -322,13 +443,48 @@ void parseInput(char *input, char *arg_arr[], int *arg_cnt, char *input_file, ch
             arg_arr[*arg_cnt] = strdup(token);
             (*arg_cnt)++;
         }
+        // Increment token
         token = strtok(NULL, " ");
     }
 }
 
-//
-void catchSIGINT(int signo)
+// Handle SIGINT signal
+void sigint_handler(int signo)
 {
-    char *message = "SIGINT detected. Exiting...\n";
-    write(STDOUT_FILENO, message, 28);
+    char *message = "terminated by signal \n";
+    write(STDOUT_FILENO, message, 22);
+    fflush(stdout);
+}
+
+// Handle SIGCHLD signal
+void sigchld_handler(int signum)
+{
+    pid_t pid;
+    int   status;
+    pid = waitpid(-1, &status, WNOHANG); // Get the pid
+    
+    if (pid > 1)
+    {
+        //printf("Child pid %d is done\n", pid);
+        //getStatus(&status); // Get the exit status
+
+        add_pid_arr(pid);
+        write(STDOUT_FILENO, "\n", 1);
+        fflush(stdout);   
+    }
+}
+
+// Toggle background-only mode on/off
+void sigtstp_handler(int signum){
+    if (B_GND == 0)
+    {
+        B_GND = 1;
+        char* message = "\nEntering foreground-only mode (& is now ignored)\n";
+	    write(STDOUT_FILENO, message, 50);
+    } else {
+
+        char* message = "\nExiting foreground-only mode\n";
+	    write(STDOUT_FILENO, message, 30);
+        B_GND = 0;
+    }
 }
